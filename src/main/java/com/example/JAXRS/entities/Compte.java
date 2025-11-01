a18530a48e20cc4b3d92a10cc93626a70db58d76
CHAKRAhossam <hchakra8@gmail.com>
2025-11-01 17:51:55 +0100

    first commit

---
+++ src/main/java/com/example/JAXRS/entities/Compte.java
@@ -0,0 +1,27 @@
+package com.example.JAXRS.entities;
+
+import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.*;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@XmlRootElement
+public class Compte {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double solde;
+
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+
+    @Enumerated(EnumType.ORDINAL)
+    private TypeCompte type;
+
+}
